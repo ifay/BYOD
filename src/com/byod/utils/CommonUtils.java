b9a11a269e1046663bbc4fc23336c18485d233c4
ifay <guyanshuangfei@gmail.com>
2014-12-19 15:42:09 +0800

    final version

--- src/com/byod/utils/CommonUtils.java
+++ src/com/byod/utils/CommonUtils.java
@@ -22,9 +22,10 @@ public class CommonUtils {
 
     public static String PKG_NAME = "com.byod";
 
-    public static String ONLINE_SERVER = "http://172.16.42.6:18080"; // TODO
-    public static String ONLINE_SERVER2 = "http://172.16.42.6:8080"; // TODO
-//    public static String ONLINE_SERVER = "http://10.0.0.16:8080"; // TODO
+//    public static String ONLINE_SERVER = "http://172.16.42.6:18080"; // TODO
+//    public static String ONLINE_SERVER2 = "http://172.16.42.6:8080"; // TODO
+    public static String ONLINE_SERVER = "http://10.0.0.16:18080"; // TODO
+    public static String ONLINE_SERVER2 = "http://10.0.0.16:8080"; // TODO
     public static String IAM_URL = ONLINE_SERVER+"/IAM/ws/webservice";
     public static String IAM_NAMESPACE = "http://inter.webservice.iam.qrry.com/";
     public static String UIA_URL = ONLINE_SERVER+"/UIA"; //TODO
